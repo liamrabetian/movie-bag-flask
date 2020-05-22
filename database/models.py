795761a6d572505cba987a7cf4819ac5626e8c05
mohammadrabetian <tommyrabetian@gmail.com>
2020-05-22 14:07:18 +0430

    authorization for movie apis

--- database/models.py
+++ database/models.py
@@ -7,14 +7,18 @@ class Movie(db.Document):
     name = db.StringField(required=True, unique=True)
     casts = db.ListField(db.StringField(), required=True)
     genres = db.ListField(db.StringField(), required=True)
+    added_by = db.RefrenceField('User')
 
 
 class User(db.Document):
     email = db.EmailField(required=True, unique=True)
     password = db.StringField(required=True)
+    movies = db.ListField(db.RefrenceField('Movie', reverse_delete_rule=db.PULL))
 
     def hash_password(self):
         self.password = generate_password_hash(self.password).decode('utf-8')
     
     def check_password(self, password):
         return check_password_hash(self.password, password)
+    
+User.register_delete_rule(Movie, 'added_by', db.CASCADE)
--- resources/movie_flask_restful.py
+++ resources/movie_flask_restful.py
@@ -1,8 +1,8 @@
 from flask import Response
 from flask_restful import Resource, request
-from flask_jwt_extended import jwt_required
+from flask_jwt_extended import jwt_required, get_jwt_identity
 
-from database import Movie
+from database import Movie, User
 
 
 class MoviesApi(Resource):
@@ -12,8 +12,12 @@ class MoviesApi(Resource):
     
     @jwt_required
     def post(self):
+        user_id = get_jwt_identity()
+        user = User.objects.get(id=user_id)
         body = request.get_json()
-        movie = Movie.objects.create(**body)
+        movie = Movie.objects.create(**body, added_by=user)
+        user.update(push__movies=movie)
+        user.save()
         return {'id': str(movie.id)}, 200
 
 
@@ -21,13 +25,17 @@ class MovieApi(Resource):
 
     @jwt_required
     def put(self, id):
+        user_id = get_jwt_identity()
+        user = User.objects.get(id=user_id)
         body = request.get_json()
-        Movie.objects.get(id=id).update(**body)
+        Movie.objects.get(id=id, added_by=user).update(**body)
         return '', 203
     
     @jwt_required
     def delete(self, id):
-        Movie.objects.get(id=id).delete()
+        user_id = get_jwt_identity()
+        user = User.objects.get(id=user_id)
+        Movie.objects.get(id=id, added_by=user).delete()
         return '', 203
     
     def get(self, id):
