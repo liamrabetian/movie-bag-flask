0bd26adfe09afc17212ad3fd9afc25a4300f2dce
mohammadrabetian <tommyrabetian@gmail.com>
2020-05-22 15:45:12 +0430

    some formating

--- resources/reset_password.py
+++ resources/reset_password.py
@@ -3,18 +3,22 @@ from flask_jwt_extended import create_access_token, decode_token
 from database.models import User
 from flask_restful import Resource
 import datetime
-from resources.errors import SchemaValidationError, InternalServerError, \
-    EmailDoesnotExistsError, BadTokenError
-from jwt.exceptions import ExpiredSignatureError, DecodeError, \
-    InvalidTokenError
+from resources.errors import (
+    SchemaValidationError,
+    InternalServerError,
+    EmailDoesnotExistsError,
+    BadTokenError,
+)
+from jwt.exceptions import ExpiredSignatureError, DecodeError, InvalidTokenError
 from services.mail_service import send_email
+
 
 class ForgotPassword(Resource):
     def post(self):
-        url = request.host_url + 'reset/'
+        url = request.host_url + "reset/"
         try:
             body = request.get_json()
-            email = body.get('email')
+            email = body.get("email")
             if not email:
                 raise SchemaValidationError
 
@@ -25,33 +29,37 @@ class ForgotPassword(Resource):
             expires = datetime.timedelta(hours=24)
             reset_token = create_access_token(str(user.id), expires_delta=expires)
 
-            return send_email('[Movie-bag] Reset Your Password',
-                              sender='support@movie-bag.com',
-                              recipients=[user.email],
-                              text_body=render_template('email/reset_password.txt',
-                                                        url=url + reset_token),
-                              html_body=render_template('email/reset_password.html',
-                                                        url=url + reset_token))
+            return send_email(
+                "[Movie-bag] Reset Your Password",
+                sender="support@movie-bag.com",
+                recipients=[user.email],
+                text_body=render_template(
+                    "email/reset_password.txt", url=url + reset_token
+                ),
+                html_body=render_template(
+                    "email/reset_password.html", url=url + reset_token
+                ),
+            )
         except SchemaValidationError:
             raise SchemaValidationError
         except EmailDoesnotExistsError:
             raise EmailDoesnotExistsError
-        except Exception as e:
+        except Exception:
             raise InternalServerError
 
 
 class ResetPassword(Resource):
     def post(self):
-        url = request.host_url + 'reset/'
+        url = request.host_url + "reset/"
         try:
             body = request.get_json()
-            reset_token = body.get('reset_token')
-            password = body.get('password')
+            reset_token = body.get("reset_token")
+            password = body.get("password")
 
             if not reset_token or not password:
                 raise SchemaValidationError
 
-            user_id = decode_token(reset_token)['identity']
+            user_id = decode_token(reset_token)["identity"]
 
             user = User.objects.get(id=user_id)
 
@@ -59,11 +67,13 @@ class ResetPassword(Resource):
             user.hash_password()
             user.save()
 
-            return send_email('[Movie-bag] Password reset successful',
-                              sender='support@movie-bag.com',
-                              recipients=[user.email],
-                              text_body='Password reset was successful',
-                              html_body='<p>Password reset was successful</p>')
+            return send_email(
+                "[Movie-bag] Password reset successful",
+                sender="support@movie-bag.com",
+                recipients=[user.email],
+                text_body="Password reset was successful",
+                html_body="<p>Password reset was successful</p>",
+            )
 
         except SchemaValidationError:
             raise SchemaValidationError
@@ -71,6 +81,5 @@ class ResetPassword(Resource):
             raise ExpiredTokenError
         except (DecodeError, InvalidTokenError):
             raise BadTokenError
-        except Exception as e:
+        except Exception:
             raise InternalServerError
-
